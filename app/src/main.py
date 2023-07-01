c59af12262aae8739e918e711f87ffdc75b70c9e
Uomonero1 <niks9498@gmail.com>
2023-07-01 09:43:22 +0200

    add error handling: input function and get coordinates function

--- app/src/main.py
+++ app/src/main.py
@@ -1,12 +1,18 @@
-from utils import get_input, get_coordinates
+from utils import get_input, get_coordinates, parse_data
 from fetch_data import get_weather
+import json
 
 
 def main():
     city = get_input()
-    lat, lon = get_coordinates(city=city)
+    try:
+        lat, lon = get_coordinates(city=city)
+    except TypeError:
+        print("An error occured with the name of the location. Check your spelling.")
+        return None
     weather = get_weather(latitude=lat, longitude=lon, city=city)
-    print(weather)
+    parse_weather = parse_data(weather)
+    print(parse_weather)
 
 
 if __name__ == "__main__":
--- app/src/utils.py
+++ app/src/utils.py
@@ -1,3 +1,4 @@
+import json
 from geopy.geocoders import Nominatim
 
 
@@ -12,5 +13,34 @@ def get_input() -> str:
 
 def get_coordinates(city: str) -> tuple:
     geolocator = Nominatim(user_agent="my-app")
-    location = geolocator.geocode(city)
-    return location.latitude, location.longitude
+    try:
+        location = geolocator.geocode(city)
+        if location is not None:
+            return location.latitude, location.longitude
+        else:
+            print("Coordinates not found")
+            return None
+    except Exception as e:
+        return None
+
+
+def parse_data(response: dict) -> dict:
+    apparent_temperature_max = {}
+    precipitation_probability_max = {}
+    for i in range(len(response["daily"]["time"])):
+        apparent_temperature_max[
+            response["daily"]["time"][i]
+        ] = f'{response["daily"]["apparent_temperature_max"][i]} Celsius'
+
+        precipitation_probability_max[
+            response["daily"]["time"][i]
+        ] = f'{response["daily"]["precipitation_probability_max"][i]} %'
+    parse_response = {}
+    parse_response["forecasts"] = []
+    parse_response["forecasts"].extend(
+        (
+            {"apparent_temperature_max": apparent_temperature_max},
+            {"precipitation_probability_max": precipitation_probability_max},
+        )
+    )
+    return json.dumps(parse_response, indent=4)
