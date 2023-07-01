d60cdda3410af6641a2db145cd54e0628afd5464
Uomonero1 <niks9498@gmail.com>
2023-07-01 12:41:40 +0200

    add command line city input option

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,30 +1,31 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
+from sys import argv
 
 
 def get_input() -> str:
-    while True:
-        city: str = input("Type the city: ").strip().capitalize()
-        if city != " ":
-            return city
-        else:
-            get_input()
+    try:
+        city = argv[1]
+        return city.strip().capitalize()
+    except IndexError:
+        city = input("Type the city: ").strip().capitalize()
+        return city
 
 
-def get_coordinates(city: str) -> tuple:
+def get_coordinates(city: str) -> tuple | None:
     geolocator = Nominatim(user_agent="my-app")
     try:
         location = geolocator.geocode(city)
         if location is not None:
             return location.latitude, location.longitude
         else:
-            print("Coordinates not found")
+            print("Coordinates not found.")
             return None
     except Exception as e:
         return None
 
 
-def parse_data(response: dict) -> dict:
+def parse_data(response: dict) -> pd.DataFrame:
     parse_response = {}
     for i in range(len(response["time"])):
         parse_response[response["time"][i]] = [
